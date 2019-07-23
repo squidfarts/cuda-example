da9f17e867ec74f7a1e52ab7bd191d6a134d04d9
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:53:12 -0700

    Add files via upload

---
+++ src/main/main.cu
@@ -0,0 +1,28 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.cu                                                                   *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+#include <iostream>
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, cuda' is desplayed to the standerd console.
+ *
+ */
+int main(void)
+{
+    puts("Hello, cuda.");
+    
+    return EXIT_SUCCESS;
+}// end of function main
